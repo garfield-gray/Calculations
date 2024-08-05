e3c46b8ee267a9d2c6b74bf5d54fe2f3799a189f
g <garfield.gray.999@gmial.com>
2024-08-05 18:10:21 -0400

    start from the ship.py

--- BabyProj/alien_invasion/alien_invasion.py
+++ BabyProj/alien_invasion/alien_invasion.py
@@ -2,13 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class"""
     def __init__(self):
         """Init the game"""
         pygame.init()
+        self.settings = Settings()
+  
+    
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invason")
 
     def run_game(self):
@@ -19,10 +25,13 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     
                     sys.exit()
+            
 
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
             # Make the mos recently drawn screen visible.
             pygame.display.flip()
-
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
