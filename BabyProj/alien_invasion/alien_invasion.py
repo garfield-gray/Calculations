628d8c9f707aadd7b20e4a9564687a157f258bab
g <garfield.gray.999@gmial.com>
2024-08-06 12:15:02 -0400

    up to refactoring

--- BabyProj/alien_invasion/alien_invasion.py
+++ BabyProj/alien_invasion/alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 class AlienInvasion:
     """Overall class"""
     def __init__(self):
@@ -16,6 +18,8 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invason")
+        
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop"""
@@ -29,6 +33,8 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # Make the mos recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
